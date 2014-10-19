dfe1c67febe3f98b422d70648064f198f5c41a7d
tintinsu <wenjia.yuan@gmail.com>
2014-10-19 12:39:35 -0700

    Create "Minimum Path Sum"
    2D DP

---
+++ Minimum Path Sum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minPathSum(vector<vector<int> > &grid) {
+        int m=grid.size();
+        if(m==0) return 0;
+        int n=grid[0].size();
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0) continue;
+                if(i==0){grid[i][j]+=grid[i][j-1]; continue;}
+                if(j==0){grid[i][j]+=grid[i-1][j]; continue;}
+                grid[i][j] += min(grid[i][j-1], grid[i-1][j]);
+            }
+        }
+        return grid[m-1][n-1];
+    }
+};
