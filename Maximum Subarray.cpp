4bfaa17d2c85ed611ff2ed057a781fd11ba61990
tintinsu <wenjia.yuan@gmail.com>
2015-01-08 20:08:52 -0500

    Update Maximum Subarray.cpp
    Optimize the space complexity

--- Maximum Subarray.cpp
+++ Maximum Subarray.cpp
@@ -11,4 +11,16 @@ public:
         }
         return max_val;
     }
+    
+    /* with O(1) space and O(n) complexity */
+    int maxSubArray_1(int A[], int n) {
+        if(n==0) return 0;
+        int last = A[0], curr, maxsub=A[0];
+        for(int i=1;i<n;i++){
+            curr = A[i] + (last>0?last:0);
+            if(curr > maxsub) maxsub = curr;
+            last = curr;
+        }
+        return maxsub;
+    }
 };
